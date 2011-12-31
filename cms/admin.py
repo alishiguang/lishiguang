2893383455c86c1dedc2aa917bc93903ac87a623
alishiguang <alishiguang@gmail.com>
2011-12-31 17:57:33 +0800

    增加 页面内容过长显示过滤 功能

--- cms/admin.py
+++ cms/admin.py
@@ -2,13 +2,18 @@
 __author__ = 'Lishiguang'
 
 from django.contrib import admin
-from lishiguang.cms.models import Catagory,Article
+from lishiguang.cms.models import Catagory, Article
+
+page_sie = 2
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('headline','catagory','pub_date')
-    list_filter = ('pub_date','catagory')
+    list_per_page = page_sie #分页条数
+    list_display = ('headline', 'catagory', 'pub_date')
+    list_filter = ('pub_date', 'catagory')
     ordering = ('-pub_date',)
+
     class Media:
-        js=('/site_media/js/tiny_mce/tiny_mce.js','/site_media/js/textareas.js')
+        js = ('/site_media/js/tiny_mce/tiny_mce.js', '/site_media/js/textareas.js')
+
 admin.site.register(Catagory)
-admin.site.register(Article,ArticleAdmin)
+admin.site.register(Article, ArticleAdmin)
